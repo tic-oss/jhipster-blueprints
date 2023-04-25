25bc7bc12dde26441246e01f6f4e7504e948c5b5
comakeit-tic <tic.dev@comakeit.com>
2023-04-25 18:49:26 +0530

    env variables

--- generator-jhipster-go/generators/server/templates/go/go/db/dbHandler.go
+++ generator-jhipster-go/generators/server/templates/go/go/db/dbHandler.go
@@ -3,16 +3,31 @@ package config
 import (   
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"   
-	"<%= packageName %>/domains"
-	"<%= packageName %>/customlogger"
-	ft "<%= packageName %>/fileutil"
+	"com.cmi.tic/domains"
+	"com.cmi.tic/customlogger"
+	"os"
+	// ft "com.cmi.tic/fileutil"
+	"log"
+	"github.com/joho/godotenv"
 )
 	
 var Database *gorm.DB
 
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+	  log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+  }
+
 func DbConnect(){   
-	props, _ := ft.ReadPropertiesFile("config.properties")
-	dsn := "host="+props["db_host"]+" user="+props["db_user"]+" dbname="+props["db_name"]+" port="+props["db_port"];
+    db_host :=goDotEnvVariable("db_host")
+    db_user :=goDotEnvVariable("db_user")
+	db_name :=goDotEnvVariable("db_name")
+	db_port :=goDotEnvVariable("db_port")
+	// props, _ := ft.ReadPropertiesFile("config.properties")
+	dsn := "host="+db_host+" user="+db_user+" dbname="+db_name+" port="+db_port;
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})   
 	Database = db   
 	if err != nil{      
